3c22b6ed1dd66e5849b3f8809da9998ab759594e
DanielaLopez66 <230415@utxicotepec.edu.mx>
2026-02-08 17:28:50 -0600

    Proyecto Autolavado

---
+++ models/modelRols.py
@@ -0,0 +1,15 @@
+# pylint: disable=invalid-name, import-error, too-few-public-methods
+"""
+Docstring for autolavado_backend.models.modelRols
+"""
+from sqlalchemy import Column, Integer, String, Boolean
+from config.db import Base
+
+
+class Rol(Base):
+    """Modelo de la tabla de roles"""
+    __tablename__ = "tbc_roles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    descripcion = Column(String(60))
+    estatus = Column(Boolean)
---
+++ models/modelUsers.py
@@ -0,0 +1,23 @@
+# pylint: disable=invalid-name, import-error, too-few-public-methods
+"""
+Docstring for autolavado_backend.models.modelUsers
+"""
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from config.db import Base
+
+
+class User(Base):
+    """Modelo de la tabla de usuarios"""
+    __tablename__ = "tbb_users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    rol_id = Column(Integer, ForeignKey("tbc_roles.id"))
+    nombre = Column(String(60))
+    papellido = Column(String(60))
+    sapellido = Column(String(60))
+    usuario = Column(String(60))
+    contrasena = Column(String(60))
+    telefono = Column(String(15))
+    estatus = Column(Boolean)
+    fecha_registro = Column(DateTime)
+    fecha_modificacion = Column(DateTime)
