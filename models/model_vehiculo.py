11a5024ffd9c02e18d07b2673b0bdafa492c4ab0
Daniela <230415@utxicotepec.edu.mx>
2026-02-09 09:16:28 -0600

    nuevo

--- models/model_vehiculo.py
+++ models/model_vehiculo.py
@@ -1,11 +1,19 @@
-'''
-Docstring for models.modelVehiculo
-'''
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+"""
+Modelo de vehículos para el sistema de autolavado.
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
 from config.db import Base
 
-class Vehiculo(Base):
-    '''Modelo del vehiculo'''
-    __table__ = "tbb_vehiculo"
 
-    id = Column
+class Vehiculo(Base):
+    """Modelo de la tabla vehiculos."""
+
+    __tablename__ = "tbb_vehiculo"
+
+    id = Column(Integer, primary_key=True, index=True)
+    matricula = Column(String(20))
+    color = Column(String(30))
+    numero = Column(String(30))
+    modelo = Column(String(40))
+    id_cliente = Column(Integer, ForeignKey("tbc_clientes.id"))
